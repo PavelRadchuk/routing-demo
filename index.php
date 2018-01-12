7326f994cd02ce61d1a05ad14697b8e18440a9dd
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-01-12 14:49:58 -0500

    added to roots

--- index.php
+++ index.php
@@ -9,8 +9,19 @@
 
     $f3 = Base::instance();
 
+    $f3->set('DEBUG', 3);
+
     $f3->route("GET /", function(){
         echo '<h1>Routing Demo</h1>';
     });
 
+    $f3->route("GET /page1", function() {
+        echo '<h1>This is page one</h1>';
+    });
+    $f3->route("GET /page2", function() {
+        echo '<h1>This is page two</h1>';
+    });
+    $f3->route("GET /page1/subpage-a", function() {
+        echo '<h1>This is page one,subpage-a</h1>';
+    });
     $f3->run();
